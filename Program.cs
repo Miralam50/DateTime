e799b030c4e61467e8afaf0f9af3e618090e8dd2
Miralam50 <miralamqs@code.edu.az>
2023-06-09 13:10:27 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Tarixi daxil edin:  ");
+                string date = Console.ReadLine();
+                var cevrilmisFormat = DateTime.ParseExact(date, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                Console.WriteLine(cevrilmisFormat);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
